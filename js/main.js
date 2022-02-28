b86198cb0eb24fb3eaadd2378e032c7f483a1f85
evelina <evelina.salisheva.96@gmail.com>
2022-03-01 02:47:10 +0500

    Файл main.js подключен к index.html. В файле main.js добавлены функции возвращения случайного числа и проверки длины комментария.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+
+function getRandomNum(min, max) {
+  if (max <= min) {
+   console.log('Ошибка'); 
+  }
+  if (min < 0) {
+   console.log('Введите число больше либо равное 0'); 
+  }
+  if (max < 0) {
+    console.log('Введите число больше либо равное 0'); }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandomNum();
+
+//Функция для проверки максимальной длины строки.
+
+
+function limitStr(str, n) {
+ if (str.length > n) {
+  console.log('warning') 
+ }
+ return str
+}
+
+limitStr();
